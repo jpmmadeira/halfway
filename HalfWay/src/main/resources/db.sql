38200e922e255829dbfe0dd528e7b221776c6552
David Monteiro <dmontdl@gmail.com>
2019-12-05 20:07:34 +0000

    Added MORE THINGS TO THE DATABASE AAAAAA

--- HalfWay/src/main/resources/db.sql
+++ HalfWay/src/main/resources/db.sql
@@ -34,3 +34,28 @@ INSERT INTO project(projectName, description, projectPicture, githubUrl) VALUES(
 'Snake', 'A one-week group project developed after classes in our “spare-time”.',
 'http://rembound.com/files/creating-a-snake-game-tutorial-with-html5/snake.png',
 'https://github.com/ACNormalTeam/Snake-Game');
+
+INSERT INTO project(projectName, description, projectPicture, githubUrl) VALUES(
+'Guess the Coder?', 'A weekend group project developed at <Academia_de_Código_>.',
+'https://theknighttimes.net/wp-content/uploads/2017/10/question-mark-face.jpg',
+'https://gitlab.com/pedrosupico/guessthecoder'
+);
+
+INSERT INTO project(projectName, description, projectPicture, githubUrl) VALUES(
+'Cadets Against Humanity!', '
+A 2-day group project developed over the weekend. Our idea was to recreate the popular card game "Cards Against Humanity" (and make it playable in the terminal), which is, a famous party icebreaker, adults-only game for players mature enough to handle the intentionally provocative (but often hilarious) topics and answers. The goal is to pair the answer and question cards in the funniest, most provocative, or the cleverest way you can.',
+'https://i5.walmartimages.com/asr/19616f7f-73b5-4fce-91ed-5a99349b7917_1.ae33d245a9c99f98f67be8e0d1c674d1.jpeg?odnWidth=undefined&odnHeight=undefined&odnBg=ffffff',
+'https://github.com/rvbrd/cadets-against-humanity'
+);
+
+INSERT INTO project(projectName, description, projectPicture, githubUrl) VALUES(
+'Quizizinho', 'A one-weekend group project developed after classes in our “spare-time”. This was the second group project at <Academia de Código_>.',
+'https://img.quizur.com/f/img5ce409067fe485.83822464.jpg?lastEdited=1558448401',
+'https://github.com/Tribelos/QuizGame'
+);
+
+INSERT INTO project(projectName, description, projectPicture, githubUrl) VALUES(
+'ShipWreck', 'A command line-run rendition of everyone''s favorite naval battle boardgame with concurrent multiplayer done over the course of 3 days.',
+'https://store-images.s-microsoft.com/image/apps.49131.68190763503362257.7b91f0f7-106b-4f3c-ad9e-a0f35fc5836b.51945ab3-cfdd-4cf1-9f80-4e9cbbcc06a0?mode=scale&q=90&h=1080&w=1920',
+'https://github.com/VillageCadets/NavalGame'
+);
