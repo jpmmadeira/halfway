ff609841832e1c07cbf23fe70f66aa7e2882064c
Danones <diogo.sousa.uk@gmail.com>
2019-12-05 21:36:24 +0000

    implemented DELETE method

---
+++ HalfWay/src/main/java/org/academiadecodigo/halfway/exceptions/ProjectNotFoundException.java
@@ -0,0 +1,8 @@
+package org.academiadecodigo.halfway.exceptions;
+
+public class ProjectNotFoundException extends HalfWayException{
+
+    public ProjectNotFoundException() {
+        super("Project not found");
+    }
+}
--- HalfWay/src/main/java/org/academiadecodigo/halfway/services/ProjectService.java
+++ HalfWay/src/main/java/org/academiadecodigo/halfway/services/ProjectService.java
@@ -1,5 +1,6 @@
 package org.academiadecodigo.halfway.services;
 
+import org.academiadecodigo.halfway.exceptions.ProjectNotFoundException;
 import org.academiadecodigo.halfway.persistence.model.Project;
 
 import java.util.List;
@@ -9,4 +10,9 @@ public interface ProjectService {
     Project get(Integer id);
 
     List<Project> list();
+
+    Project save(Project project);
+
+    void delete(Integer id) throws ProjectNotFoundException;
+
 }
